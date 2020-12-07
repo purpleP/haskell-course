886fee4b9c4cc77e42bbc9a9c0231d36d17f4de5
Michael Doronin <warrior2031@mail.ru>
2020-12-07 18:30:32 +0300

    wip

--- haskell/src/SimpleFunctions.hs
+++ haskell/src/SimpleFunctions.hs
@@ -15,9 +15,10 @@ merge left@(l:ls) right@(r:rs) = if l <= r
 minmax :: (Ord a) => NonEmpty a -> (a, a)
 minmax (x :| xs) = minmax' (x, x) xs where
     minmax' (lo, hi) [] = (lo, hi)
-    minmax' (lo, hi) [y] = if y < lo
-                              then (y, hi)
-                              else if y > hi then (lo, y) else (lo, hi)
+    minmax' (lo, hi) [y]
+        | y < lo = (y, hi)
+        | y > hi = (lo, y)
+        | otherwise = (lo, hi)
     minmax' (lo, hi) (y1:y2:ys) = minmax' (lo', hi') ys where
         (l, h) = if y1 <= y2 then (y1, y2) else (y2, y1)
         lo' = if l < lo then l else lo
